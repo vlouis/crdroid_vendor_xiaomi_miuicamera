fb3da4999e4156408b2ec2cf487a95240508cf73
vlouis <40323785+vlouis@users.noreply.github.com>
2024-07-08 16:01:27 +0300

    Update config.mk

--- config.mk
+++ config.mk
@@ -2,7 +2,8 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/xiaomi/miuicamera
 
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/etc,$(TARGET_COPY_OUT_SYSTEM)/etc)
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/etc,$(TARGET_COPY_OUT_SYSTEM)/etc) \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/priv-app/MiuiCamera/lib,$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiCamera/lib)
 
 PRODUCT_PACKAGES += \
     MiuiCamera
